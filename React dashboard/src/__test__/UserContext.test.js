af8ad884359d292998f1b609e5808e2a6d672a82
alfettox <dott.defranceschi@gmail.com>
2023-10-04 22:14:53 -0400

    update book

---
+++ React dashboard/src/__test__/UserContext.test.js
@@ -0,0 +1,32 @@
+/**
+ * @jest-environment jsdom
+ */
+import { expect, test } from "@jest/globals";
+import { render, screen } from "@testing-library/react";
+import { StaticRouter } from "react-router-dom/server";
+import Header from "../components/layout/Header.jsx"
+import { UserContext } from "../client/context/UserContext.jsx"
+
+
+test("displays the correct Context Value", async function () {
+  // ARRANGE
+  const wrapper = function ({ children }) {
+    const user = {
+      storeNumber: "003",
+      userName: "Brooklyn,NY",
+    };
+    return <UserContext.Provider value={user}>{children}</UserContext.Provider>;
+  };
+  render(
+    <StaticRouter>
+      <Header />
+    </StaticRouter>,
+    { wrapper }
+  );
+
+  // ACT
+  const welcomeUser = await screen.getByText(/^Store/).textContent;
+
+  // ASSERT
+  expect(welcomeUser).toBe("Store 003");
+});
